c062b2cd4ccc9e5b43b10f6e4474eb82eba34101
anamasiliuniene <ana.masiliuniene@gmail.com>
2026-01-27 19:42:37 +0200

    sukurtas ir registruotas Order modelis

--- mysite/autoservice/models.py
+++ mysite/autoservice/models.py
@@ -11,9 +11,18 @@ class Car(models.Model):
         return f"{self.license_plate} ({self.make})"
 
 class Order(models.Model):
+    STATUS_CHOICES = [
+        ("open", "Open"),
+        ("in_progress", "In progress"),
+        ("done", "Done"),
+    ]
+
     date = models.DateTimeField(auto_now_add=True)
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
-    status = models.CharField(max_length=20, default="open")
-
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default="open"
+    )
     def __str__(self):
-        return f"Order #{self.id} – {self.car}"
+        return f" {self.car} - ({self.date:%Y-%m-%d}) - {self.status}"
