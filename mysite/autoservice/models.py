594e726e3348b98dd1c2c46142c4ac7f43dbb701
anamasiliuniene <ana.masiliuniene@gmail.com>
2026-01-27 19:30:09 +0200

    sukurtas ir registruotas COrder modelis

--- mysite/autoservice/models.py
+++ mysite/autoservice/models.py
@@ -9,3 +9,11 @@ class Car(models.Model):
 
     def __str__(self):
         return f"{self.license_plate} ({self.make})"
+
+class Order(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, default="open")
+
+    def __str__(self):
+        return f"Order #{self.id} – {self.car}"
