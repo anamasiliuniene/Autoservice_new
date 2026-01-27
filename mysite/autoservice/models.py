ebc6d04616698751f5882137c27e3fd5ff4bd7b4
anamasiliuniene <ana.masiliuniene@gmail.com>
2026-01-27 19:26:19 +0200

    sukurtas ir registruotas Car modelis

--- mysite/autoservice/models.py
+++ mysite/autoservice/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    make = models.CharField()
+    model = models.CharField()
+    license_plate = models.CharField()
+    client_name = models.CharField()
+
+    def __str__(self):
+        return f"{self.license_plate} ({self.make})"
