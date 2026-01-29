ab5c26c689a7f0bf546b5833b149d83b8891d357
anamasiliuniene <ana.masiliuniene@gmail.com>
2026-01-29 14:52:51 +0200

    Atlikta pirmojo puslapio uzduotis

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Car, Order, Service, OrderLine
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'autoservice/index.html')
+    context = {
+        "num_cars": Car.objects.count(),
+        "num_services": Service.objects.count(),
+        "num_order_done": Order.objects.filter(status='done').count(),
+        "num_order_in_progress": Order.objects.filter(status='in_progress').count(),
+        "num_order_open": Order.objects.filter(status='open').count(),
+    }
+    return render(request, 'autoservice/index.html', context=context)
